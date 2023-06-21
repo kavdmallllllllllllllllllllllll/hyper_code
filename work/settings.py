9a1c5552da5e420fa9c8d9ee0b7807d68f1c49e6
kamal <97139355+kavdmallllllllllllllllllllllll@users.noreply.github.com>
2023-06-22 00:46:50 +0300

    Update settings.py

--- work/settings.py
+++ work/settings.py
@@ -139,3 +139,18 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGIN_REDIRECT_URL = "/"
+
+
+
+
+
+EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+
+EMAIL_HOST_USER = 'ka6051152@gmail.com'
+EMAIL_HOST_PASSWORD = 'nbdjplowdukrnqhe'
+EMAIL_USE_TLS = True
+EMAIL_PORT = '587'
