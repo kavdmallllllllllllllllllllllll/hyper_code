ee3ed2c0396829166a04b682bbea14a6fc474eed
kamal <97139355+kavdmallllllllllllllllllllllll@users.noreply.github.com>
2023-06-22 00:48:03 +0300

    Update admin.py

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import cluns,massgee,images
 # Register your models here.
+
+
+admin.site.register(cluns)
+admin.site.register(massgee)
+admin.site.register(images)
