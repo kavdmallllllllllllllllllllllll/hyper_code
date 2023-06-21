70690d419df075db023096f9bcfed942fc5df597
kamal <97139355+kavdmallllllllllllllllllllllll@users.noreply.github.com>
2023-06-22 00:48:50 +0300

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,20 @@ from django.urls import path
 urlpatterns = [
 
     path('', views.home,name='home'),
+    path('login/', views.loginn,name='login'),
+    path('login_out/', views.login_out,name='login_out'),
+
+    path('my_profil/', views.profil,name='profil'),
+
+    path('contact/', views.contact,name='contact'),
+
+
+
+    path('signup/', views.sign_up,name='singup'),
+
+
+    
+
+    
 ]
 
